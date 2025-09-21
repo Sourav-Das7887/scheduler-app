acd9b2470be640d33b59b3e803de9131c30c1de9
Sourav Das <dassourav845@gmail.com>
2025-09-22 00:45:58 +0530

    1

--- backend/src/knexfile.ts
+++ backend/src/knexfile.ts
@@ -1,23 +1,33 @@
+import type { Knex } from "knex";
 
-import type { Knex } from 'knex';
-
-const config: {[key: string]: Knex.Config} = {
-    development: {
-        client: "pg",
-        connection: {
-            host: process.env.DB_HOST || "127.0.0.1",
-            port:  Number(process.env.DB_PORT || 5432),
-            user: process.env.DB_USER || "postgres",
-            password: process.env.DB_PASSWORD || "1234",
-            database: process.env.DB_NAME || "scheduler_db",
-        },
-        migrations: {
-            directory: "./migrations",
-        },
-        seeds: {
-            directory: "./seeds",
-        },
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: Number(process.env.DB_PORT || 5432),
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "1234",
+      database: process.env.DB_NAME || "scheduler_db",
     },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL, // Render provides this
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
 };
 
 export default config;
